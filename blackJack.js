d0137f5ec9ed0d4ee5ecae4dda8936168b87de69
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-06 15:38:51 -0500

    api information is added as well as css styling. code has bug, and event listener is incorrect, but the needed elements to finish are there

--- blackJack.js
+++ blackJack.js
@@ -1,32 +1,60 @@
 document.addEventListener("DOMContentLoaded",()=>{
-// let button = document.createElement("button")
-// button.innerText = "Draw Card"
-// document.body.appendChild(button)
-// let divLeft = document.createElement("div")
-// let divRight = document.createElement("div")
 
+let button = document.createElement("button")
 
-    const getCards = async () => {
-        try {
-            let res = await axios.get("https://deckofcardsapi.com/api/deck/dct9y7o6wtke/draw/?count=2");
-
-let cardData = Object.values(res)[0].cards
-
-cardData.forEach(el => {
-let selectImages =   el.image
-let img = document.createElement("img")
-img.src = selectImages
-document.body.appendChild(img)
-})
-} catch(err) {
-    console.log(err);
-    debugger
-    
+const getGame = async () => {
+    try {
+        let res =  await axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1");
+     debugger;
+       let getCards = res.data.cards
+        debugger;
+  
+     debugger
+        getCards.forEach(el => {
+           
+    let card = document.createElement("div")
+    let button = document.querySelector("button")
+    button.innerText = el.value
+    let img = document.createElement("img")
+    img.src = el.image;
+    let h2 = document.createElement("h2")
+    h2.innerText = ""
+        if(el.value === "QUEEN" || "KING" || "JACK" ){
+            let h2 = document.querySelector
+            h2.innerText = 10 
+        }else if( parseInt(el.value)<= 10){
+            let h2 = document.querySelector("h2")
+            h2.innerText = parseInt(el.value)
+  }
+    card.appendChild(h2)
+    card.appendChild(img)
+    document.body.appendChild(card)
+    document.body.appendChild(button)
+        })
+    } catch(err) {
+        console.log(err);
+        debugger    
+    }
 }
-}
-
-getCards()
+getGame()
+button.addEventListener("click",()=>{
+    getGame()
 })
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
